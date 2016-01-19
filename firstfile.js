66036e71374a228b93046959d7b327fde19abe22
Siddhu <siddhu@cliqr.com>
2016-01-19 16:56:19 +0530

    new js file added

---
+++ firstfile.js
@@ -0,0 +1,15 @@
+function a(){
+	//sample javascript file
+}
+
+function b(){
+	//sample javascript file
+}
+
+function c(){
+	//sample javascript file
+}
+
+function d(){
+	//sample javascript file
+}
