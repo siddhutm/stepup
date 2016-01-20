1f8b22f9ecf27872ccead30beda0aa98ca70d743
Siddhu <siddhu@cliqr.com>
2016-01-20 10:34:09 +0530

    feature commit 1

--- firstfile.js
+++ firstfile.js
@@ -1,9 +1,15 @@
 function a(){
 	//sample javascript file
+	// modifying changes for feature development
+	var a,b;
+	console.log("output values of a and b",a,b);
 }
 
 function b(){
 	//sample javascript file
+	// modifying changes for feature development
+	var c,d;
+	console.log("output values of c and d",c,d);
 }
 
 function c(){
