bb8656514774b099e8c1eedcf592aaa642ff1e67
Siddhu <siddhu@cliqr.com>
2016-01-20 10:39:49 +0530

    some development happened in master while feature is in progress.

--- firstfile.js
+++ firstfile.js
@@ -1,9 +1,13 @@
 function a(){
 	//sample javascript file
+	// some changes happened in master meanwhile
+	console.log("changes done in master for method a ");
 }
 
 function b(){
 	//sample javascript file
+	// some changes happened in master meanwhile
+	console.log("changes done in master for method b ");
 }
 
 function c(){
@@ -12,4 +16,24 @@ function c(){
 
 function d(){
 	//sample javascript file
+}
+
+function e(){
+
+}
+
+function f(){
+	
+}
+
+function g(){
+	
+}
+
+function h(){
+	
+}
+
+function i(){
+	
 }
