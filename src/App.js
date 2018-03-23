e9b7ed641c76f29573691f4b8e2d9e56ebca5335
siddhutm <siddhu@cliqr.com>
2018-03-23 19:57:04 +0530

    bootstrapping react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Name from './components/Name';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Name/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Name.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Name = () => {
+	return (
+		<div>Siddhu</div>
+	) 
+};
+
+export default Name;
